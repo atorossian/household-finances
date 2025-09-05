b03f72234dca395e79a49b5c6e0e3dfc8bdbbdaa
Alejandro Daniel Torossian <alejandro.torossian@gmail.com>
2025-09-05 17:42:04 +0200

    Update entries.py

--- app/api/entries.py
+++ app/api/entries.py
@@ -22,12 +22,12 @@ def create_entry(payload: EntryCreate, user=Depends(get_current_user)):
 
     # Check account membership
     if not ((str(account_memberships["user_id"]) == str(payload.user_id)) &
-            (str(account_memberships["account_id"]) == str(account_id))).any():
+            (str(account_memberships["account_id"]) == str(account_id))):
         raise HTTPException(status_code=403, detail="Account mismatch or not assigned to user")
 
     # Check household membership
     if not ((str(household_memberships["user_id"]) == str(payload.user_id)) &
-            (str(household_memberships["household_id"]) == str(household_id))).any():
+            (str(household_memberships["household_id"]) == str(household_id))):
         raise HTTPException(status_code=403, detail="Household mismatch or not assigned to user")
 
     
