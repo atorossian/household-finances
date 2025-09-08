c191dd6ba93fceebcbb589238f09a3a2d3b15ecf
Alejandro Daniel Torossian <alejandro.torossian@gmail.com>
2025-09-08 22:48:07 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.api import entries, users, household, accounts, summaries
 app = FastAPI()
 
 app.include_router(entries.router, prefix="/entries", tags=["Entries"])
+app.include_router(entries.router, prefix="/debts", tags=["Debts"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(household.router, prefix="/households", tags=["Households"])
 app.include_router(accounts.router, prefix="/accounts", tags=["Accounts"])
